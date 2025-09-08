24fc6e243724c89cb34e912b1379ae82da71d879
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-09-08 23:58:07 +0000

    Add Drawer for favorite screen and categories.

--- App.jsx
+++ App.jsx
@@ -5,9 +5,21 @@ import CategoryScreen from './screens/CategoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import MealsOverView from './screens/MealsOverView';
 import MealDetails from './screens/MealDetails';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoriteScreen from './screens/FavoriteScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator style={screenOptions} screenOptions={{drawerContentStyle: '#7cc0d5ff', drawerInactiveTintColor: 'gray', drawerActiveTintColor: 'black'}}>
+      <Drawer.Screen name="Categories" component={CategoryScreen} options={{drawerIcon: ({color, size}) => <Ionicons name='list' color={color} size={size}/>}}/>
+      <Drawer.Screen name="FavoriteScreen" component={FavoriteScreen} options={{drawerIcon: ({color, size}) => <Ionicons name='star' color={color} size={size} />}}/>
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -15,10 +27,7 @@ export default function App() {
       <StatusBar style="dark" />
       <Stack.Navigator
         screenOptions={screenOptions}>
-        <Stack.Screen name="MealCategories" component={CategoryScreen}
-          options={{
-            title: 'Meal Categories'
-          }} />
+        <Stack.Screen name="Drawer" component={DrawerNavigator} options={{ title: 'All Categories', headerShown: false}}/>
         <Stack.Screen name="Meal" component={MealsOverView} />
         <Stack.Screen name="MealDetails" component={MealDetails} />
       </Stack.Navigator>
@@ -26,13 +35,6 @@ export default function App() {
 
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
-
 
 const screenOptions = {
   headerStyle: {
