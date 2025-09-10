8d7df0a2bc55d89222c59606b63149625a636281
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-09-10 17:06:52 +0000

    Finish Favorite Screen.

--- screens/FavoriteScreen.jsx
+++ screens/FavoriteScreen.jsx
@@ -1,14 +1,38 @@
 import MealList from "../components/MealList";
 import { useFavoriteContext } from "../store/context/favorite-context";
 import { MEALS } from "../data/dummy-data";
-import { useMemo } from "react";
+import { useLayoutEffect, useMemo } from "react";
+import { Text, StyleSheet } from "react-native";
 
-export default function FavoriteScreen({navigation}) {
+export default function FavoriteScreen({ navigation }) {
     const { favs } = useFavoriteContext();
     const mealLookup = useMemo(() => Object.fromEntries(MEALS.map(meal => [meal.id, meal])), [MEALS]);
-     const handlePress = (id) => {
-        navigation.navigate('MealDetails', {mealId : id})
+    const handlePress = (id) => {
+        navigation.navigate('MealDetails', { mealId: id })
     }
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: 'Favorite Meals'
+        });
+    }, []);
     const favoriteMeals = useMemo(() => favs.map(fav => mealLookup[fav]), [favs, mealLookup]);
-    return <MealList meals={favoriteMeals} handlePress={handlePress}/>;
+    return (
+        <>
+            {
+                favoriteMeals.length > 0 ? <MealList meals={favoriteMeals} handlePress={handlePress} /> : <View style={styles.container}><Text style={styles.text}>No favorite meals selected.</Text></View>
+            }
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+})
