3cc5461a44021ac40872a98df53aa18125539063
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-09-10 01:21:33 +0000

    Add press handler for favorite screen.

--- screens/FavoriteScreen.jsx
+++ screens/FavoriteScreen.jsx
@@ -3,9 +3,12 @@ import { useFavoriteContext } from "../store/context/favorite-context";
 import { MEALS } from "../data/dummy-data";
 import { useMemo } from "react";
 
-export default function FavoriteScreen() {
+export default function FavoriteScreen({navigation}) {
     const { favs } = useFavoriteContext();
     const mealLookup = useMemo(() => Object.fromEntries(MEALS.map(meal => [meal.id, meal])), [MEALS]);
+     const handlePress = (id) => {
+        navigation.navigate('MealDetails', {mealId : id})
+    }
     const favoriteMeals = useMemo(() => favs.map(fav => mealLookup[fav]), [favs, mealLookup]);
-    return <MealList meals={favoriteMeals} />;
+    return <MealList meals={favoriteMeals} handlePress={handlePress}/>;
 }
